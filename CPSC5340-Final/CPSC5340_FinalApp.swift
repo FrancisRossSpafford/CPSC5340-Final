2c6710aa7bd8d5b514dc14fd4602ead1d40f67a7
Francis Ross Spafford <spafffr@auburn.edu>
2025-04-26 13:04:18 -0500

    Initial Commit

---
+++ CPSC5340-Final/CPSC5340_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPSC5340_FinalApp.swift
+//  CPSC5340-Final
+//
+//  Created by Ross Spafford on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CPSC5340_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPSC5340-Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPSC5340-Final
+//
+//  Created by Ross Spafford on 4/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
